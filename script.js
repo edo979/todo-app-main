aba768169d89d72b520aa7947788a38ffdb720e2
edo979 <prezentabilan@gmail.com>
2022-06-18 08:56:15 +0200

    add new todos

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 document.addEventListener('alpine:init', () => {
   Alpine.data('todosApp', () => ({
     todos: [],
+    id: 0,
+    newTodoText: '',
+
+    getId() {
+      return this.id++
+    },
+
+    addTodo() {
+      this.todos.push({
+        id: this.getId(),
+        text: this.newTodoText,
+        isCompleted: false,
+      })
+
+      this.newTodoText = ''
+    },
   }))
 })
